8064f314e4ce7340a941dc061d89bd8ff6409412
Tom Robertson <tomasrobertson@ymail.com>
2014-03-28 16:17:27 +0000

    working with logarithmic chunking

---
+++ part2/haskell/euler.hs
@@ -0,0 +1,22 @@
+import Control.Parallel.Strategies
+
+hcf x 0 = x
+hcf x y = hcf y (rem x y)
+
+relprime x y = hcf x y == 1
+
+eulersum m = length (filter (relprime m) [1 .. m-1])
+
+euler xs = sum (map eulersum xs)
+
+split xs = split' 2 xs
+split' m [] = []
+split' m xs = ( take (getl m) xs ):( split' (m + 1) (drop (getl m) xs) )
+
+getl l = abs(floor(logBase 1.1 l))
+
+
+sumTotient lower upper = sum (parMap rpar euler (split (reverse [lower, lower+1 .. upper])))
+
+
+main = print (sumTotient 1 30000)
